1c042371d1adbc4c6721105bdbda9fbba20e7c84
AbrilPal <abrilpal99@gmail.com>
2022-08-06 18:51:20 -0600

    regula_falsi function

--- proyecto.py
+++ proyecto.py
@@ -7,10 +7,14 @@
 # Descripcion: es un método cerrado para encontrar las raices de una función. Este metodo toma como punto medio el cruce con el eje x al trazar una
 # linea secante definida por los puntos de la función en los extremos del intervalo.  Al evaluar el punto medio encontrado y los intervalos,  se debe
 # escoger las funciones que den signos diferentes y descartar el otro extremo que tenga el mismo signo.  Con estos nuevos intervalos se traza una 
-# nueva línea. Este proceso se realiza sucesivamente hasta llegar a la linea tangente de la función dada; por lo tanto, el punto de tangencia es la 
-# raíz.
+# nueva línea. Este proceso se realiza sucesivamente hasta llegar a la linea tangente de la función dada; por lo tanto, el punto de tangencia es la raíz.
 
 #Lector de funcion
+import math
+from wsgiref.validate import InputWrapper
+import numpy as np
+import matplotlib.pyplot as plt
+from tabulate import tabulate
 from sympy import var
 from sympy import sympify
 
@@ -21,7 +25,7 @@ def functionLexer():
     print("""
     1. Sum = '+'
     2. Substract = '-'
-    3. Multiply = '*'
+    3. Multiply = 
     4. Division = '/'
     5. Power = '**'
     6. Root = '**(1/n)'
@@ -38,4 +42,59 @@ def functionLexer():
         valor3 = input('Ingrese el valor de z: ')
         res1 = res1.subs(z, valor3)
     print(res1)
-functionLexer()
+
+def regula_falsi():
+    operadores = [
+        ['suma', ' + '],
+        ['Resta', ' - '],
+        ['Multiplicación', ' * '],
+        ['División', ' / '],
+        ['Exponente', ' ** '],
+        ['Raiz', ' **(1/n) '],
+        ['Prioridad', ' () ']
+    ]
+
+    # header de la tabla
+    head = ["Operación", "input"]
+    print()
+    print("         Tabla No.1")
+    print(tabulate(operadores, headers=head, tablefmt="grid"))
+    print()
+    # Ingreso de la funcion, a, b y tolerancia
+    fx = lambda x: math.exp(-x) * -x
+    a = float(input("Ingrese a: "))
+    b = float(input("Ingrese b: "))
+    tolera = float(input("Ingrese la tolerancia del error: "))
+
+    tabla = []
+    puntos = abs(b-a)
+    fa = fx(a)
+    fb = fx(b)
+    while not(puntos <= tolera):
+        # Para hallar la intersección de la recta con el eje X
+        c = b - fb * (a - b ) / (fa - fb)
+        fc = fx(c)
+        tabla.append([a,c,b,fa,fc,fb,puntos])
+        # cambio de signo
+        cambio = np.sign(fa)*np.sign(fc)
+        if cambio>0:
+            puntos = abs(c-a)
+            a = c
+            fa = fc
+        else:
+            puntos = abs(b-c)
+            b = c
+            fb = fc
+            
+    ntabla = len(tabla)
+
+    head = ["a", "c", "b", "f(a)", "f(c)", "f(b)", "margen de error"]
+    print()
+    print("                                              Tabla No.2")
+    print(tabulate(tabla, headers=head, tablefmt="grid"))
+    print()
+    print("Número de iteraciones: ", ntabla)
+    print("Raiz: ", c)
+    print("Error: ", puntos)
+
+regula_falsi()
