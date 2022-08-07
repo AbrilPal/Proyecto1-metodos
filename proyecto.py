fcfd37c45bbf58b566f7d0145308941971e48002
AbrilPal <abrilpal99@gmail.com>
2022-08-06 19:21:18 -0600

    comentarios

--- proyecto.py
+++ proyecto.py
@@ -5,13 +5,12 @@
 # Fecha: 08/07/2022
 
 # Descripcion: es un método cerrado para encontrar las raices de una función. Este metodo toma como punto medio el cruce con el eje x al trazar una
-# linea secante definida por los puntos de la función en los extremos del intervalo.  Al evaluar el punto medio encontrado y los intervalos,  se debe
+# linea secante definida por los punto de la función en los extremos del intervalo.  Al evaluar el punto medio encontrado y los intervalos,  se debe
 # escoger las funciones que den signos diferentes y descartar el otro extremo que tenga el mismo signo.  Con estos nuevos intervalos se traza una 
 # nueva línea. Este proceso se realiza sucesivamente hasta llegar a la linea tangente de la función dada; por lo tanto, el punto de tangencia es la raíz.
 
 #Lector de funcion
 import math
-from wsgiref.validate import InputWrapper
 import numpy as np
 import matplotlib.pyplot as plt
 from tabulate import tabulate
@@ -54,35 +53,41 @@ def regula_falsi():
     tolera = float(input("Ingrese la tolerancia del error: "))
 
     tabla = []
-    puntos = abs(b-a)
+    punto = abs(b-a)
+    # evaluar en el punto a
     fa = functionLexer(fun,a)
+    # evaluar en el punto b
     fb = functionLexer(fun,b)
-    while not(puntos <= tolera):
+    while not(punto <= tolera):
         # Para hallar la intersección de la recta con el eje X
         c = b - fb * (a - b ) / (fa - fb)
+        # evaluar en la interseccion, c
         fc = functionLexer(fun,c)
-        tabla.append([a,c,b,fa,fc,fb,puntos])
+        # se complio una iteracion asi que se agrega a la matriz, tabla
+        tabla.append([a,c,b,fa,fc,fb,punto])
         # cambio de signo
         cambio = np.sign(fa)*np.sign(fc)
         if cambio>0:
-            puntos = abs(c-a)
+            punto = abs(c-a)
             a = c
             fa = fc
         else:
-            puntos = abs(b-c)
+            punto = abs(b-c)
             b = c
             fb = fc
-            
+    # para saber la cantidad de iteraciones        
     ntabla = len(tabla)
 
+    # mostrar tabla de iteraciones
     head = ["a", "c", "b", "f(a)", "f(c)", "f(b)", "margen de error"]
     print()
     print("                                    Tabla No.2")
     print(tabulate(tabla, headers=head, tablefmt="grid"))
     print()
+    # RESULTADOS
     print("Número de iteraciones: ", ntabla)
     print("Raiz: ", c)
-    print("Error: ", puntos)
+    print("Error: ", punto)
     print()
 
 regula_falsi()
